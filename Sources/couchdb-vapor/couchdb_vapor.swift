826a761248781f690968ac6997a18e539fd75df7
Sergey Armodin <dolph-in@yandex.ru>
2019-03-06 00:36:03 +0300

    Get method added for CouchDBClient

--- Sources/couchdb-vapor/couchdb_vapor.swift
+++ Sources/couchdb-vapor/couchdb_vapor.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import HTTP
 
 
 class CouchDBClient: NSObject {
@@ -24,13 +25,38 @@ class CouchDBClient: NSObject {
 	/// Base URL
 	private var couchBaseURL: String = ""
 
-	
+
+	// MARK: - Init
 	override init() {
 		super.init()
 		
 		self.couchBaseURL = self.buildBaseUrl()
 	}
 	
+	
+	// MARK: - Public methods
+	func get(dbName: String, uri: String, query: [String: Any]? = nil, worker: Worker) -> Future<HTTPResponse>? {
+		let client = HTTPClient.connect(
+			scheme: .http,
+			hostname: couchHost,
+			port: couchPort,
+			connectTimeout: TimeAmount.seconds(30),
+			on: worker
+		) { (error) in
+			print(error)
+		}
+		
+		let queryString = buildQuery(fromQuery: query)
+		
+		let url = self.couchBaseURL + "/" + dbName + "/" + uri + queryString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+		
+		return client.flatMap { (httpCli) -> Future<HTTPResponse> in
+			let httpReq = HTTPRequest(
+				method: .GET,
+				url: url)
+			return httpCli.send(httpReq)
+		}
+	}
 }
 
 
@@ -40,5 +66,22 @@ private extension CouchDBClient {
 	/// - Returns: Base URL string
 	private func buildBaseUrl() -> String {
 		return "\(self.couchProtocol)\(self.couchHost):\(self.couchPort)"
+	}
+	
+	/// Build query string
+	///
+	/// - Parameter query: params dictionary
+	/// - Returns: query string
+	func buildQuery(fromQuery query: [String: Any]?) -> String {
+		var queryString = ""
+		
+		if query != nil {
+			var strings = [String]()
+			for (key, value) in query! {
+				strings.append("\(key)=\(value)")
+			}
+			queryString = "?\(strings.joined(separator: "&"))"
+		}
+		return queryString
 	}
 }
