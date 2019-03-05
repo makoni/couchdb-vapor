79f4acb3c58f095b7a5f51fcc43df0ad4b1eaa69
Sergey Armodin <dolph-in@yandex.ru>
2019-03-06 00:47:56 +0300

    CouchUpdateResponse codable.

--- Sources/couchdb-vapor/CouchUpdateResponse.swift
+++ Sources/couchdb-vapor/CouchUpdateResponse.swift
@@ -8,8 +8,27 @@
 import Foundation
 
 
-struct CouchUpdateResponse {
+struct CouchUpdateResponse: Codable {
 	var ok: Bool
 	var id: String
 	var rev: String
+	
+	enum CodingKeys: String, CodingKey {
+		case ok
+		case id
+		case rev
+	}
+}
+
+
+extension CouchUpdateResponse {
+	init(from decoder: Decoder) throws {
+		let container = try decoder.container(keyedBy: CodingKeys.self)
+		
+		let ok = try container.decodeIfPresent(Bool.self, forKey: .ok)
+		let id = try container.decodeIfPresent(String.self, forKey: .id)
+		let rev = try container.decodeIfPresent(String.self, forKey: .rev)
+		
+		self.init(ok: ok ?? false, id: id ?? "", rev: rev ?? "")
+	}
 }
