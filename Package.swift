26d4e73b7b818084c7f9c18026961ecdfcefb129
Sergey Armodin <dolph-in@yandex.ru>
2019-03-06 00:35:40 +0300

    Added vapor HTTP dependency to Package.swift

--- Package.swift
+++ Package.swift
@@ -4,25 +4,25 @@
 import PackageDescription
 
 let package = Package(
-    name: "couchdb-vapor",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "couchdb-vapor",
-            targets: ["couchdb-vapor"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "couchdb-vapor",
-            dependencies: []),
-        .testTarget(
-            name: "couchdb-vaporTests",
-            dependencies: ["couchdb-vapor"]),
-    ]
+	name: "couchdb-vapor",
+	products: [
+		// Products define the executables and libraries produced by a package, and make them visible to other packages.
+		.library(
+			name: "couchdb-vapor",
+			targets: ["couchdb-vapor"]),
+		],
+	dependencies: [
+		// 🚀 Non-blocking, event-driven HTTP for Swift built on Swift NIO.
+		.package(url: "https://github.com/vapor/http.git", from: "3.0.0"),
+	],
+	targets: [
+		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
+		// Targets can depend on other targets in this package, and on products in packages which this package depends on.
+		.target(
+			name: "couchdb-vapor",
+			dependencies: ["HTTP"]),
+		.testTarget(
+			name: "couchdb-vaporTests",
+			dependencies: ["couchdb-vapor"]),
+		]
 )
