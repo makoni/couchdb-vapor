70af3897b71492a8783c03d4d8e075078eb16c95
Sergey Armodin <dolph-in@yandex.ru>
2019-03-06 00:13:57 +0300

    CouchDBClient class added

--- Sources/couchdb-vapor/couchdb_vapor.swift
+++ Sources/couchdb-vapor/couchdb_vapor.swift
@@ -1,3 +1,44 @@
-struct couchdb_vapor {
-    var text = "Hello, World!"
+//
+//  couchdb_vapor.swift
+//  couchdb-vapor
+//
+//  Created by Sergey Armodin on 06/03/2019.
+//
+
+import Foundation
+
+
+class CouchDBClient: NSObject {
+	
+	// MARK: - Private properties
+	
+	/// Protocol
+	private var couchProtocol: String = "http://"
+	
+	/// Host
+	private var couchHost: String = "127.0.0.1"
+	
+	/// Port
+	private var couchPort: Int = 5984
+	
+	/// Base URL
+	private var couchBaseURL: String = ""
+
+	
+	override init() {
+		super.init()
+		
+		self.couchBaseURL = self.buildBaseUrl()
+	}
+	
+}
+
+
+private extension CouchDBClient {
+	/// Build Base URL
+	///
+	/// - Returns: Base URL string
+	private func buildBaseUrl() -> String {
+		return "\(self.couchProtocol)\(self.couchHost):\(self.couchPort)"
+	}
 }
