a15c8cd223db384eadd225eee3628d4e54c46d82
Sergey Armodin <dolph-in@yandex.ru>
2019-03-06 11:50:24 +0300

    Files and targets renamed

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
 	products: [
 		// Products define the executables and libraries produced by a package, and make them visible to other packages.
 		.library(
-			name: "couchdb-vapor",
-			targets: ["couchdb-vapor"]),
+			name: "CouchDBClient",
+			targets: ["CouchDBClient"]),
 		],
 	dependencies: [
 		// 🚀 Non-blocking, event-driven HTTP for Swift built on Swift NIO.
@@ -19,10 +19,10 @@ let package = Package(
 		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
 		// Targets can depend on other targets in this package, and on products in packages which this package depends on.
 		.target(
-			name: "couchdb-vapor",
+			name: "CouchDBClient",
 			dependencies: ["HTTP"]),
 		.testTarget(
-			name: "couchdb-vaporTests",
-			dependencies: ["couchdb-vapor"]),
+			name: "CouchDBClientTests",
+			dependencies: ["CouchDBClient"]),
 		]
 )
