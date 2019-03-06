5862df63f4f457f2a0f20990af52c6979fab9a0e
Sergey Armodin <dolph-in@yandex.ru>
2019-03-06 12:20:31 +0300

    fixes

--- Tests/CouchDBClientTests/couchdb_vaporTests.swift
+++ Tests/CouchDBClientTests/couchdb_vaporTests.swift
@@ -1,12 +1,12 @@
 import XCTest
-@testable import couchdb_vapor
+@testable import CouchDBClient
 
 final class couchdb_vaporTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(couchdb_vapor().text, "Hello, World!")
+//        XCTAssertEqual(CouchDBClient().text, "Hello, World!")
     }
 
     static var allTests = [
