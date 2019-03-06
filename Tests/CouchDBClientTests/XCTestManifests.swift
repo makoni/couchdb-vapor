4eeb1bd623c6bf6d3610eb1b92b27538572855b8
Sergey Armodin <dolph-in@yandex.ru>
2019-03-06 12:35:53 +0300

    ...

--- Tests/CouchDBClientTests/XCTestManifests.swift
+++ Tests/CouchDBClientTests/XCTestManifests.swift
@@ -3,7 +3,7 @@ import XCTest
 #if !os(macOS)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(couchdb_vaporTests.allTests),
+        testCase(CouchDBClientTests.allTests),
     ]
 }
 #endif
