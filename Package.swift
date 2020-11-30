89a6ff019ab376ada986bccc008ffa51204d0eb5
Sergey Armodin <dolph-in@yandex.ru>
2020-11-30 13:18:53 +0300

    Package.swift updated

--- Package.swift
+++ Package.swift
@@ -1,22 +1,27 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-	name: "couchdb-vapor",
-	products: [
-		.library(name: "CouchDBClient",targets: ["CouchDBClient"]),
-	],
-	dependencies: [
-		.package(url: "https://github.com/swift-server/async-http-client.git", from: "1.1.1")
-	],
-	targets: [
-		.target(
-			name: "CouchDBClient",
-			dependencies: ["AsyncHTTPClient"]),
-		.testTarget(
-			name: "CouchDBClientTests",
-			dependencies: ["CouchDBClient"]),
-		]
+    name: "couchdb-vapor",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(name: "CouchDBClient", targets: ["CouchDBClient"]),
+    ],
+    dependencies: [
+		.package(url: "https://github.com/swift-server/async-http-client.git", from: "1.0.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "CouchDBClient",
+            dependencies: [
+				.product(name: "AsyncHTTPClient", package: "async-http-client")
+			]),
+        .testTarget(
+            name: "CouchDBClientTests",
+            dependencies: ["CouchDBClient"]),
+    ]
 )
