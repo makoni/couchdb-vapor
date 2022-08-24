9c0117295f78cbaf4e0dd848067f2f42cd7993c5
Sergey Armodin <dolph-in@yandex.ru>
2022-08-24 22:09:18 +0300

    swift 5.6. minimum required versions are macOS 10.15, iOS 13

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
 	name: "couchdb-vapor",
-	platforms: [.macOS(.v12), .iOS(.v15)],
+	platforms: [.macOS(.v10_15), .iOS(.v13)],
 	products: [
 		// Products define the executables and libraries a package produces, and make them visible to other packages.
 		.library(name: "CouchDBClient", targets: ["CouchDBClient"]),
