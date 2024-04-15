a57f6ad6649ab138b3008dd797738444cd9d87e8
Sergey Armodin <dolph-in@yandex.ru>
2024-04-16 00:05:38 +0300

    Vapor tutorial updated

---
+++ Sources/CouchDBClient/CouchDBClient.docc/Tutorials/vapor/VaporTutorial-package-2.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.10
+import PackageDescription
+
+let package = Package(
+	name: "spaceinbox.me",
+	platforms: [
+	   .macOS(.v13)
+	],
+	dependencies: [
+		// 💧 A server-side Swift web framework.
+		.package(url: "https://github.com/vapor/vapor", from: "4.0.0"),
+		.package(url: "https://github.com/vapor/leaf", from: "4.0.0"),
+		.package(url: "https://github.com/makoni/couchdb-vapor", from: "1.5.0")
+	],
+	targets: [
+		.target(
+			name: "App",
+			dependencies: [
+				.product(name: "Leaf", package: "leaf"),
+				.product(name: "Vapor", package: "vapor"),
+				.product(name: "CouchDBClient", package: "couchdb-vapor")
+			]
+		),
+		.executableTarget(name: "Run", dependencies: [.target(name: "App")]),
+		.testTarget(name: "AppTests", dependencies: [
+			.target(name: "App"),
+			.product(name: "XCTVapor", package: "vapor"),
+		])
+	]
+)
