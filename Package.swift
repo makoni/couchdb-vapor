21cbfc65a29c217aaf75d79b454b2cfd5a347036
Sergey <dolph-in@yandex.ru>
2024-09-19 23:08:30 +0300

    1.6.2 (#24)
    * swift-docc-plugin 1.3.0
    
    * async-http-client 1.19.0
    
    * replaced deprecated .createNew with the new .singleton
    
    * Adding _find capability. (#13)
    
    * docs
    
    * tabs
    
    * renames
    
    * added test for find method with body
    
    * docs added
    
    * findError added
    
    * tests renamed
    
    * added test for find method with generic type
    
    * docs
    
    * README updated
    
    * updated dependencies
    
    * docs updated
    
    * try Swift 5.7.1
    
    * Update build-ubuntu.yml
    
    * Swift 5.7.3
    
    * Revert "Swift 5.7.3"
    
    This reverts commit ab8f67a89f5b83ed20fe30a9965a3f2f58f6d2c1.
    
    * updated dependencies
    
    * Docs updated
    
    * Delete .github/workflows/CodeQL.yml
    
    * dependencies updated
    
    * fixed when update method didn’t use passed dateEncodingStrategy
    
    * noData error added
    
    * migrating to new HTTPClientRequest from HTTPClient.Request wip
    
    * migrating to new HTTPClientRequest from HTTPClient.Request wip
    
    * migrating to new HTTPClientRequest from HTTPClient.Request wip
    
    * migrating to new HTTPClientRequest from HTTPClient.Request wip
    
    * migrating to new HTTPClientRequest from HTTPClient.Request wip
    
    * migrating to new HTTPClientRequest from HTTPClient.Request wip
    
    * …
    
    * migrating to new HTTPClientRequest from HTTPClient.Request wip
    
    * rename
    
    * tests updated
    
    * auth fixed
    
    * collect body bytes before returning response
    
    * param renamed to make keep backward compatibility with old methods
    
    * marking old find as deprecated
    
    * private
    
    * deprecated message
    
    * renames
    
    * moved deprecations
    
    * docs
    
    * docs
    
    * added Codable to CouchDBRepresentable protocol
    
    * added RowsResponse model
    
    * docs
    
    * Vapor tutorial updated
    
    * docs
    
    * docs and refactoring
    
    * docs and refactoring
    
    * Tutorials updated
    
    * docs
    
    * platforms list updated
    
    * minimum swift version 5.8
    
    * workflow updated
    
    * Update build-ubuntu.yml
    
    * Update build-macos.yml
    
    * import NIOFoundationCompat to fix building on Ubuntu
    
    * using async-http-client from 1.21.0. Not calling http
    
    * Using HTTPClient.shared if eventLoopGroup not provided. No more calls httpClient.syncShutdown() if using shared singleton
    
    * docs updated
    
    * docs updated
    
    * Vapor tutorial updated
    
    * A SwiftUI tutorial
    
    * tutorial fixed
    
    * updated dependencies
    
    * Package.resolved
    
    * docs updated
    
    * userName param is now required for CouchDB init
    
    * cleanup
    
    * docs
    
    * docs
    
    * docs
    
    * docs
    
    * docs
    
    * docs
    
    * docs
    
    * docs
    
    * docs
    
    * docs
    
    * docs
    
    * docs
    
    * docs
    
    * docs
    
    * docs
    
    * docs
    
    * dependencices updated
    
    * updated swift-docc-plugin URL. Updated dependencies to latest versions.
    
    * Update build-ubuntu.yml
    
    ---------
    
    Co-authored-by: Gregório Gevartosky Torrezan <greg.gevartosky@gmail.com>

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
 	],
 	dependencies: [
 		.package(url: "https://github.com/swift-server/async-http-client.git", from: "1.21.0"),
-		.package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.3.0")
+		.package(url: "https://github.com/swiftlang/swift-docc-plugin.git", from: "1.3.0")
 	],
 	targets: [
 		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
